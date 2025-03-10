ab03ce5c44a26651f90c312a9fb1fe41ab4adb10
cesar3600 <cesar.contreras.medina.3600@gmail.com>
2025-03-10 07:51:37 -0500

    feat(style): added styles for themes dark and light on login

--- shell/src/components/Pages/Login.tsx
+++ shell/src/components/Pages/Login.tsx
@@ -1,18 +1,34 @@
-//import logo from "@assets/pokelogo.png";
-
 import logo from "@assets/pokelogo.png";
+import { SetStateAction, useState } from "react";
 
-const Login = () => {
+interface onChangeThemeProps {
+  target: { value: SetStateAction<string> };
+}
+
+const Login: React.FC = () => {
+  const [theme, setTheme] = useState("light");
+
+  const onChangeTheme = (e: onChangeThemeProps) => {
+    setTheme(e.target.value);
+  };
+
+  const toggleTheme = theme === "light" ? "light" : "dark";
+
   return (
-    <div className="login-form">
+    <div className={`login-form theme-${toggleTheme}`}>
       <div className="login-form__container">
         <img className="login-form__logo" src={logo} alt="" />
         <form>
           <div className="form-group">
             <label htmlFor="">theme:</label>
-            <select name="" id="">
-              <option value="claro">claro</option>
-              <option value="oscuro">oscuro</option>
+            <select
+              name="theme"
+              id="theme"
+              value={theme}
+              onChange={onChangeTheme}
+            >
+              <option value="light">claro</option>
+              <option value="dark">oscuro</option>
             </select>
           </div>
           <div className="form-group">
